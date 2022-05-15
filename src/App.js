9670ff853ed43b30b91e452598121f9ddb98b00c
bhankymyk <bankole123michael@gmail.com>
2022-05-15 18:39:26 +0100

    app,js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import './App.css';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Button} from 'react-bootstrap';
+import MealList from './Component/MealList';
+// import { useState } from 'react';
 
 function App() {
+  const [mealData, setMealData] = useState (null)
+  const [calories, setCalories] = useState (2000)
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=8c171682aa064b2d9b5a88c13168267e&timeFrame=day&targetCalories=${calories}`
+    )
+    .then((response) => response.json())
+    .then((data) => {
+      setMealData(data);
+    })
+    .catch(() => {
+      console.log("error");
+    })
+  }
+
+  function handleChange (e) {
+    setCalories(e.target.value)
+  } 
   return (
-    <div className="App">
-     React
-    </div>
+    <Container>
+<Row>
+<div className="App">
+      <div className=''>
+        <input type="number" placeholder="Calories" onChange={handleChange}/>
+      <Button onClick={getMealData} className="btn btn-success btn-block" >
+Get daily meal plan
+      </Button>
+      </div>
+      {mealData && <MealList mealData={mealData} />}
+      </div>
+  </Row>      
+    </Container>
+    
+    
   );
 }
 
---
+++ src/Component/MealList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Meal from './Meal'
+
+function MealList() {
+  return (
+    <div>MealList</div>
+  )
+}
+
+export default MealList
