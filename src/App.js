703868b63ffbacfba7fa4f31d8677f20706ed30f
bhankymyk <bankole123michael@gmail.com>
2024-02-07 17:57:18 +0100

    fix: toggle button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-// import { Route} from 'react-router-dom';
-import {Route} from "react-router-dom";
-// import MealList from './Component/MealList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {Route} from "react-router-dom";
 import LandingPage from './Component/LandingPage';
 import HomePage from './Component/HomePage';
 
