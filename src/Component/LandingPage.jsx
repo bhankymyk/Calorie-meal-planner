c471848fd3cc88327f3de2a00dad0e08f4b357dd
bhankymyk <bankole123michael@gmail.com>
2024-02-07 03:40:23 +0100

    fix: failed to build

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React from 'react';
 import { Container, Row} from 'react-bootstrap';
 import styles from '../Component/Assests/LandingPage.module.css';
